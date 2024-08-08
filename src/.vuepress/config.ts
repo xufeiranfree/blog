7ccf74bf48ed63ec6dcfec967ae71c82afe58e21
xufeiranfree <xu.feiran@qq.com>
2024-08-08 06:40:00 +0000

    google ad

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,7 +4,10 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
-  head: [['meta', { name: 'google-site-verification', content: 'q2k6tX5Cr-G8xLqj8gd6y1d972NEjmmAqC-LTYPdTcs' }]],
+  head: [
+    ['meta', { name: 'google-site-verification', content: 'q2k6tX5Cr-G8xLqj8gd6y1d972NEjmmAqC-LTYPdTcs' }],
+    ['script', {async: 'async', src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4703735193930324', crossorigin: 'anonymous'}]
+  ],
   locales: {
     "/": {
       lang: "en-US",
